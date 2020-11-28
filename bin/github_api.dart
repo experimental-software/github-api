9b361bc16a82647e7885f7544e1743a5c14056ae
Jan Mewes <jan.mewes@experimental-software.com>
2020-11-28 09:44:11 +0100

    Initial commit

---
+++ bin/github_api.dart
@@ -0,0 +1,5 @@
+import 'package:github_api/github_api.dart' as github_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_api.calculate()}!');
+}
---
+++ test/github_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:github_api/github_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
